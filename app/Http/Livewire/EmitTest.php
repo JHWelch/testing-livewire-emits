f496d1a32b27f567b8bea9c05858a149e339b0cd
Jordan Welch <jwelch@highlandsolutions.com>
2021-07-20 12:40:31 -0500

    Add test page

---
+++ app/Http/Livewire/EmitTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class EmitTest extends Component
+{
+    public $variable = 'x';
+
+    protected $listeners = [
+        'emitted-event' => 'changeVariable',
+    ];
+
+    public function emitEvent()
+    {
+        $this->emit('emitted-event');
+    }
+
+    public function changeVariable()
+    {
+        $this->variable = 'y';
+    }
+}
---
+++ resources/views/livewire/emit-test.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <button wire:click="emitEvent">Emit to object</button>
+    <span>{{ $variable }}</span>
+</div>
